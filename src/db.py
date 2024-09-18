4ce9d7906beccdaabc4ca19a4d8cd31ce7aba5f1
Lucas <lucas.nieto@mi.unc.edu.ar>
2024-09-17 22:15:18 -0300

    entidad partida

---
+++ src/db.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+db_url = "sqlite:///database.db"
+engine = create_engine(db_url)
+
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()  
+
+def create_table(table_name):
+    table = Base.metadata.tables.get(table_name)
+    if table is not None:
+        table.create(engine)
+    else:
+        print(f'{table_name} no existe')
---
+++ src/models/partida.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, String, Integer, Boolean, CheckConstraint
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+class Partida(Base):
+    __tablename__ = "partidas"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nombre = Column(String(50), nullable=False)
+    cantidad_max_jugadores = Column(Integer, nullable=False)
+    cantidad_min_jugadores = Column(Integer, nullable=False)
+    jugador_en_turno = Column(Integer, default=0)
+    partida_iniciada = Column(Boolean, default=False)
+
+    def __repr__(self) -> str:
+        id = f'id={self.id!r}'
+        nombre = f'nombre={self.nombre!r}'
+        cantidad_jugadores = f'cantidad_max_jugadores={self.cantidad_max_jugadores!r}, cantidad_min_jugadores={self.cantidad_min_jugadores!r}'
+        jugador_en_turno = f'jugador_en_turno={self.jugador_en_turno!r}'
+        partida_iniciada = f'partida_iniciada={self.partida_iniciada}'
+        return id + ', ' + nombre + ', ' + cantidad_jugadores + ', ' + jugador_en_turno + ', ' + partida_iniciada
+    
+    __table_args__ = (
+        CheckConstraint('cantidad_max_jugadores BETWEEN 2 AND 4', name='cantidad_max_check'),
+        CheckConstraint('cantidad_min_jugadores BETWEEN 2 AND 4', name='cantidad_min_check'),
+    )
