a452483bdf78cee3c7a20f50f963e81633ac12c3
lucas-mateo-acosta <mateo.acosta@mi.unc.edu.ar>
2024-09-27 11:16:40 -0300

    test corregido

--- tests/test_agregar_jugador.py
+++ tests/test_agregar_jugador.py
@@ -6,7 +6,7 @@ from src.models.cartamovimiento import MovementCard
 from src.models.fichas_cajon import FichaCajon
 from src.models.jugadores import Jugador
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, patch, ANY
 from sqlalchemy.exc import OperationalError
 import pdb
 from src.main import app 
@@ -37,19 +37,21 @@ def check_post_method(client: client, player_mock_id: int, game_id: int):
                                 "game_id": game_id} 
 
 def test_join_many_players(client):
-    #with patch('src.main.get_db'):
-        game_mock = MagicMock()
-        game_mock.return_value = Partida(id= 1, game_name= "Partida", max_players= 4)
+    with patch('src.main.add_player_game') as mock_jugador:
+        mock_jugador.return_value = Jugador(id= 1, nombre= "testjoins")
+        mock_jugador.return_value.partida_id = 1
+        response = client.post("/game/join", json={"player_id": 1 , "game_id": 1})
+        assert response.json() == {"player_id": 1, "game_id": 1}
 
-        with patch('src.main.add_player_game'):
-            with patch('src.consultas.get_Jugador') as mock_jugador:
-                mock_jugador.return_value = Jugador(id= 1, nombre= "testjoins")
-                response = client.post("/game/join", json={"player_id": 1 , "game_id": game_mock.return_value.id})
-                assert response.json() == {"player_id": 1, "game_id": 1}
+        mock_jugador.return_value = Jugador(id= 2, nombre= "testjoins")
+        mock_jugador.return_value.partida_id = 1
+        response1 = client.post("/game/join", json={"player_id": 2 , "game_id": 1})
+        assert response1.json() == {"player_id": 2, "game_id": 1}
 
-                mock_jugador.return_value = Jugador(id= 2, nombre= "testjoins")
-                response = client.post("/game/join", json={"player_id": 2 , "game_id": game_mock.return_value.id})
-                assert response.json() == {"player_id": 2, "game_id": 1}
+        mock_jugador.return_value = Jugador(id= 3, nombre= "testjoins")
+        mock_jugador.return_value.partida_id = 1
+        response1 = client.post("/game/join", json={"player_id": 2 , "game_id": 1})
+        assert response1.json() == {"player_id": 3, "game_id": 1}
 
 def test_login_failure(client):
     with patch('src.main.get_db'):
