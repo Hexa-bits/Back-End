310e6de993ef167a668809ff559776e1f388069b
lucas-mateo-acosta <mateo.acosta@mi.unc.edu.ar>
2024-10-10 12:12:58 -0300

    arreglo en test get-winner

--- tests/test_get_winner.py
+++ tests/test_get_winner.py
@@ -9,21 +9,6 @@ from src.main import app
 def client():
     return TestClient(app)
 
-def test_not_winner(client):
-    with patch('src.main.get_jugadores') as mock_get_jugadores:
-        game_id = 1
-        jugadores_mock = [
-            MagicMock(id=1, nombre="testwinner"),
-            MagicMock(id=2, nombre="testwinner"),
-            MagicMock(id=3, nombre="testwinner")
-        ]
-
-        mock_get_jugadores.return_value = jugadores_mock
-
-        response = client.get("/game/get-winner", params={"game_id": game_id})
-
-        assert response.status_code == 204
-
 def test_get_winner(client):
     with patch('src.main.get_jugadores') as mock_get_jugadores:
         game_id = 1
@@ -36,7 +21,7 @@ def test_get_winner(client):
         response = client.get("/game/get-winner", params={"game_id": game_id})
 
         assert response.status_code == 200
-        assert response.json() == {"id_player": 1, "name_player": "testwinner"}
+        assert response.json() == {"name_player": "testwinner"}
 
 def test_except_winner(client):
     with patch('src.main.get_jugadores') as mock_get_jugadores:
