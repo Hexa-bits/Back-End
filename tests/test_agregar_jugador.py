d0dcf201f58548ae06992f74e3d8abe380a9a9b8
lucas-mateo-acosta <mateo.acosta@mi.unc.edu.ar>
2024-11-10 20:37:04 -0300

    agrego tests para unirse con password

--- tests/test_agregar_jugador.py
+++ tests/test_agregar_jugador.py
@@ -30,6 +30,39 @@ def test_join_success(client):
                     assert response.status_code == 200
                     assert response.json() == {"player_id": player_mock.return_value.id,
                                         "game_id": 1}
+                    
+def test_join_password_success(client):
+    # Mock de la sesión de la base de datos
+    with patch('src.db.get_db'): 
+        player_mock = MagicMock()
+        player_mock.return_value = Jugador(id= 1, nombre= "testjoin")
+        
+        with patch('src.routers.game.add_player_game'):
+            with patch('src.routers.game.block_manager.add_player', return_value= None):
+                with patch('src.routers.game.get_Partida', return_value= Partida(id= 1, game_name="partida", max_players= 4,
+                                                                                 password="U2FsdGVkX19Rw2m4lWQF8GsXaRT9h/OOM7e2MK8tKyE=")):
+                    response = client.post("/game/join", json={"player_id": player_mock.return_value.id , "game_id": 1, 
+                                                                "game_password": "U2FsdGVkX19Rw2m4lWQF8GsXaRT9h/OOM7e2MK8tKyE="})    
+    
+                    assert response.status_code == 200
+                    assert response.json() == {"player_id": player_mock.return_value.id,
+                                        "game_id": 1}
+                    
+def test_join_password_failure(client):
+    # Mock de la sesión de la base de datos
+    with patch('src.db.get_db'): 
+        player_mock = MagicMock()
+        player_mock.return_value = Jugador(id= 1, nombre= "testjoin")
+        
+        with patch('src.routers.game.add_player_game'):
+            with patch('src.routers.game.block_manager.add_player', return_value= None):
+                with patch('src.routers.game.get_Partida', return_value= Partida(id= 1, game_name="partida", max_players= 4,
+                                                                                 password="U2FsdGVkX19Rw2m4lWQF8GsXaRT9h/OOM7e2MK8tKyE=")):
+                    response = client.post("/game/join", json={"player_id": player_mock.return_value.id , "game_id": 1, 
+                                                                "game_password": "U2FsdGVkX19Rw2m4lWQF8GsXaRT9h/OOM7e2MK8tKyE0"})    
+    
+                    assert response.status_code == 401
+                    assert response.json() == {"detail": "Contraseña incorrecta"}
 
 def test_join_many_players(client):
     with patch('src.routers.game.add_player_game') as mock_jugador:
