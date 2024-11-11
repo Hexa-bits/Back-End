06b7762f9b669e388e5fed357902df7bd994540d
lucas-mateo-acosta <mateo.acosta@mi.unc.edu.ar>
2024-11-11 13:59:55 -0300

    arreglo bug

--- src/routers/game.py
+++ src/routers/game.py
@@ -28,23 +28,26 @@ list_patterns = [np.array(patron) for patron in list_patterns]
 active_timers = {}
 
 async def timer(game_id: int, db: Session):
-    
-    await asyncio.sleep(120)
+
+    await asyncio.sleep(10)
     #-------
-    player = get_current_turn_player(game_id, db)
+    jugador = get_current_turn_player(game_id, db)
         
     while game_manager.is_board_parcial(game_id):
         mov_coords = game_manager.top_tuple_card_and_box_cards(game_id)
         mov = mov_coords [0]
         coords = (mov_coords [1][0], mov_coords [1][1])
-
-        cancel_movement(game_id, player.id, mov, coords, db)
+        cancel_movement(game_id, jugador.id, mov, coords, db)
         game_manager.pop_card_and_box_card(game_id)
-    repartir_cartas(game_id, db)
-    next_player = terminar_turno(game_id, db)
-    game_manager.set_player_in_turn_id(game_id=game_id, player_id=next_player["id_player"])
+    player_blocked = block_manager.is_blocked(game_id, jugador.id)
+    repartir_cartas(game_id, player_blocked, db)
+    next_jugador = terminar_turno(game_id, db)
+    #TO DO: ver si quitar jugador en turno de game_manager
+    game_manager.set_player_in_turn_id(game_id=game_id, player_id=next_jugador["id_player"])
+    
     await ws_manager.send_end_turn(game_id)
-    await timer_handler(game_id, db) 
+    await ws_manager.send_turn_log(game_id, jugador.nombre)
+    await timer_handler(game_id, db)
     #--------  esto es lo mismo que en el endpoint end-turn, hay que modularizarlo a services o a otro lado
 
 async def timer_handler(game_id: int, db: Session):
